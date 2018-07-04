10c5a754f68b92ef0ea4139454c756e385c65fc7
Chao Fan <fanchao05@gmail.com>
2018-07-03 21:26:11 -0400

    played around with map

--- map/main.go
+++ map/main.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
+
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#4bf745",
+		"white": "ffffff",
 	}
 
-	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
